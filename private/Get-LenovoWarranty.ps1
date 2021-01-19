8c0d75effc12b90acb4655ee8c4626914b063476
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2021-01-19 10:15:00 +0100

    Updated Lenovo to work via warrantyapiproxy.

--- private/Get-LenovoWarranty.ps1
+++ private/Get-LenovoWarranty.ps1
@@ -1,17 +1,16 @@
 function get-LenovoWarranty([Parameter(Mandatory = $true)]$SourceDevice, $client) {
     $today = Get-Date -Format yyyy-MM-dd
-    $APIURL = "https://ibase.lenovo.com/POIRequest.aspx"
-    $SourceXML = "xml=<wiInputForm source='ibase'><id>LSC3</id><pw>IBA4LSC3</pw><product></product><serial>$SourceDevice</serial><wiOptions><machine/><parts/><service/><upma/><entitle/></wiOptions></wiInputForm>"
-    $Req = Invoke-RestMethod -Uri $APIURL -Method POST -Body $SourceXML -ContentType 'application/x-www-form-urlencoded'
+    $APIURL = "https://warrantyapiproxy.azurewebsites.net/api/Lenovo?Serial=$SourceDevice"
+    $Req = Invoke-RestMethod -Uri $APIURL -Method get
     if ($req.wiOutputForm) {
-        $warlatest = $Req.wiOutputForm.warrantyInfo.serviceInfo.wed | sort-object | select-object -last 1 
+        $warlatest = $Req.EndDate | sort-object | select-object -last 1 
         $WarrantyState = if ($warlatest -le $today) { "Expired" } else { "OK" }
          
         $WarObj = [PSCustomObject]@{
-            'Serial'                = $Req.wiOutputForm.warrantyInfo.machineinfo.serial
-            'Warranty Product name' = $Req.wiOutputForm.warrantyInfo.machineinfo.productname -join "`n"
-            'StartDate'             = $Req.wiOutputForm.warrantyInfo.serviceInfo.warstart | sort-object -Descending | select-object -last 1
-            'EndDate'               = $Req.wiOutputForm.warrantyInfo.serviceInfo.wed | sort-object | select-object -last 1
+            'Serial'                = $Req.Serial
+            'Warranty Product name' = $Req.WarProduct
+            'StartDate'             = $Req.StartDate
+            'EndDate'               = $Req.EndDate
             'Warranty Status'       = $WarrantyState
             'Client'                = $Client
         }
